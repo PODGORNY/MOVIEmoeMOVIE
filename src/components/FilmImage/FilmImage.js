eafac6a605e8a73ed973f3d5e9be82b202a3b25d
PODGORNY <oneminutesplease@gmail.com>
2023-08-16 21:48:21 +0300

    fix

--- src/components/FilmImage/FilmImage.js
+++ src/components/FilmImage/FilmImage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Space, Spin } from 'antd';
 import PropTypes from 'prop-types';
+
 import './FilmImage.css';
+import noPoster from './no-poster.png';
 
 export default class FilmImage extends React.Component {
   constructor(props) {
@@ -27,11 +29,7 @@ export default class FilmImage extends React.Component {
         onLoad={this.isLoaded}
         className={`card__poster${posterPath ? ' card__poster--loaded' : ''}`}
         alt="Film Image"
-        src={
-          posterPath
-            ? `https://image.tmdb.org/t/p/w500${posterPath}`
-            : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'
-        }
+        src={posterPath ? `https://image.tmdb.org/t/p/w500${posterPath}` : noPoster}
       />
     );
 
